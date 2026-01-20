ebcabce0c271be94909104ef51877409656faaa0
Julian D. Michels <jdmichelsphd@gmail.com>
2026-01-20 20:39:09 +0800

    finicky

--- init.py
+++ init.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 client = OpenAI(
-    base_url="https://models.github.ai",
+    base_url="https://models.github.ai", /v1
     api_key=os.environ.get("GITHUB_TOKEN"), 
 )
 
@@ -15,7 +15,7 @@ response = client.chat.completions.create(
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": "What is the capital of France?"},
     ],
-    model="openai/gpt-4o",
+    model="openai/gpt-4o", 
 )
 
 print(response.choices[0].message.content)
